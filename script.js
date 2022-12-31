c30198ac0c9f73207b4dfef094201d65f6eca145
Expectum <118871738+Expectum@users.noreply.github.com>
2022-12-31 10:40:35 -0300

    Add operate function

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ return a * b;
 function divide(a, b) {
 return a / b;
 }
+function operate(a, operator, b) {
+switch(operator) {
+    case '+': 
+              add(a, b);  
+              break;
+    case '-': 
+              subtract(a, b);  
+              break;
+    case '*': 
+              multiply(a, b);  
+              break;
+    case '/': 
+              divide(a, b);  
+              break;
+    default:
+              console.log("Something went horribly wrong..."); 
+}
+}
