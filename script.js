6a937d5cf3b401b65d1fb547c41ce6083403f47a
Expectum <118871738+Expectum@users.noreply.github.com>
2022-12-31 20:46:46 -0300

    Add a variable for each number and a display value

--- script.js
+++ script.js
@@ -28,3 +28,14 @@ switch(operator) {
               console.log("Something went horribly wrong..."); 
 }
 }
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
+const six = document.querySelector('.six');
+const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
+const nine = document.querySelector('.nine');
+const zero = document.querySelector('.zero');
+let displayValue = 0;
